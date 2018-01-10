1c65d3445ecb1dcbc54e58de8f6122b60242be72
JAYADHARGITHUB <35135589+JAYADHARGITHUB@users.noreply.github.com>
2018-01-10 16:39:04 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int islowercasevowel,isuppercasevowel;
+printf("enter an alphabets:");
+scanf("%c",&c);
+if(is lowercase vowel)
+printf("%c is a vowel");
+else
+printf("%c is a consonants");
+}
