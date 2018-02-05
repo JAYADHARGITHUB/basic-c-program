3ad7967078bfe978a446888518d0803b57703853
JAYADHARGITHUB <35135589+JAYADHARGITHUB@users.noreply.github.com>
2018-02-05 16:12:23 +0530

    Create arms.c

---
+++ arms.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int multiple=0,num,n,digit,i;
+printf("Enter the number:");
+scanf("%d",&num);
+  n=num;
+while(n!=0)
+{
+digit=n%10;
+multiple=multiple+(digit*digit*digit);
+n=n/10;
+}
+if(multiple==num)
+{
+  printf("yes");
+}
+  else
+  {
+    printf("no");
+  }
+return 0;
+}
