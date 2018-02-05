4c711ee389826604dd94d124d8b282b7cb9f4b24
JAYADHARGITHUB <35135589+JAYADHARGITHUB@users.noreply.github.com>
2018-02-05 16:20:11 +0530

    Create multi.c

---
+++ multi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+longint sum=0,v,p;
+printf("\n Please Give The Value of N:  ");
+scanf("%ld",&p);
+for(v=1;v<=p;v++)
+{
+printf("\n 5 * %ld = %ld",i,5*v);
+}
+getch();
+}
