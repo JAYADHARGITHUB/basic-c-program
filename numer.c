857dfc890925e6ef811821d0d003ab04ffe6b009
JAYADHARGITHUB <35135589+JAYADHARGITHUB@users.noreply.github.com>
2018-02-07 15:46:07 +0530

    Rename numeric.c to numer.c

---
+++ numer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char ab;
+	scanf("%c",&ab);
+	if(ab=='0'||ab=='1'||ab=='2'||ab=='3'||ab=='4'||ab=='5'||ab=='6'||ab=='7'||ab=='8'||ab=='9')
+	{
+		printf("yes");
+
+	}
+else
+{
+	printf("no");
+}
+}
